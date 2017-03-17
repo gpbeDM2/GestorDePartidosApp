99c95f296923e084af5158eb4131b165e5ad8145
gpbeDM2 <gpbe.dm2@gmail.com>
2017-03-17 13:05:29 +0100

    Main,login,listadoInvitado

--- app/src/main/java/com/example/mikel/gestordepartidos/MainActivity.java
+++ app/src/main/java/com/example/mikel/gestordepartidos/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.mikel.gestordepartidos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    protected void invitado(View v){
+        Intent intent = new Intent(MainActivity.this, ListadoInvitado.class);
+        startActivity(intent);
+    }
+    public void usuario(View v){
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
