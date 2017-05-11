26ab475d054cf77f229bb81a22bfe41245d10f8d
JonOchoadeAlda <j.ochoalda@gmail.com>
2017-05-11 22:23:00 +0200

    11/05

--- app/src/main/java/com/example/mikel/gestordepartidos/crearPartido.java
+++ app/src/main/java/com/example/mikel/gestordepartidos/crearPartido.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class crearPartido extends AppCompatActivity {
 
-    private EditText nuevoHora, nuevoMinutos;
+    private TextView nuevoHora, nuevoMinutos;
     private Button horaAbajo, horaArriba, minutosAbajo, minutosArriba, confirmarPartido;
     private final int HORA_MAX = 23;
     private final int HORA_MIN = 0;
-    private final int MINUTOS_MAX = 60;
+    private final int MINUTOS_MAX = 59;
     private final int DOS_CIFRAS = 9;
     private int hora, minutos;
 
@@ -21,8 +22,8 @@ public class crearPartido extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_partido);
 
-        nuevoHora = (EditText) findViewById(R.id.nuevo_hora);
-        nuevoMinutos = (EditText) findViewById(R.id.nuevo_minutos);
+        nuevoHora = (TextView) findViewById(R.id.nuevo_hora);
+        nuevoMinutos = (TextView) findViewById(R.id.nuevo_minutos);
         horaAbajo = (Button) findViewById(R.id.hora_abajo);
         horaArriba = (Button) findViewById(R.id.hora_arriba);
         minutosAbajo = (Button) findViewById(R.id.minuto_abajo);
@@ -31,18 +32,8 @@ public class crearPartido extends AppCompatActivity {
         horaArriba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hora = Integer.parseInt(nuevoHora.getText().toString());
-                hora = +1;
-                if (hora > DOS_CIFRAS) {
-                    nuevoHora.setText(hora);
-                } else {
-                    nuevoHora.setText("0" + hora);
-                }
+                subirHora();
 
-                if (hora > HORA_MAX) {
-                    hora = 0;
-                    nuevoHora.setText("00");
-                }
             }
         });
 
@@ -50,19 +41,71 @@ public class crearPartido extends AppCompatActivity {
         horaAbajo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hora = Integer.parseInt(nuevoHora.getText().toString());
-                hora = -1;
-                if (hora <= DOS_CIFRAS) {
-                    nuevoHora.setText("0"+hora);
-                } else {
-                    nuevoHora.setText(hora);
-                }
 
-                if (hora > HORA_MIN) {
-                    hora = 23;
-                    nuevoHora.setText("23");
+                bajarHora();
+
+            }
+        });
+
+        minutosAbajo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minutos = Integer.parseInt(nuevoMinutos.getText().toString());
+                minutos = minutos - 5;
+                if (minutos < HORA_MIN) {
+                    minutos = 55;
+                    bajarHora();
+                }
+                if (minutos > DOS_CIFRAS) {
+                    nuevoMinutos.setText("" + minutos);
+                } else {
+                    nuevoMinutos.setText("0" + minutos);
                 }
             }
         });
+
+        minutosArriba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minutos = Integer.parseInt(nuevoMinutos.getText().toString());
+                minutos = minutos + 5;
+                if (minutos > MINUTOS_MAX) {
+                    minutos = 00;
+                    subirHora();
+                }
+                if (minutos > DOS_CIFRAS) {
+                    nuevoMinutos.setText("" + minutos);
+                } else {
+                    nuevoMinutos.setText("0" + minutos);
+                }
+            }
+        });
+    }
+
+    public void subirHora() {
+        hora = Integer.parseInt(nuevoHora.getText().toString());
+        hora = hora + 1;
+        if (hora > HORA_MAX) {
+            hora = 0;
+
+        }
+        if (hora > DOS_CIFRAS) {
+            nuevoHora.setText(hora + "");
+        } else {
+            nuevoHora.setText("0" + hora);
+        }
+    }
+
+    public void bajarHora() {
+        hora = Integer.parseInt(nuevoHora.getText().toString());
+        hora = hora - 1;
+        if (hora < HORA_MIN) {
+            hora = 23;
+        }
+        if (hora > DOS_CIFRAS) {
+            nuevoHora.setText("" + hora);
+        } else {
+            nuevoHora.setText("0" + hora);
+        }
     }
 }
