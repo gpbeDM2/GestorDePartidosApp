cf1cef381f1285d6dffbbfdebd611c9aa3c2d5fe
gpbeDM2 <gpbe.dm2@gmail.com>
2017-05-14 20:56:03 +0200

    .

--- app/src/main/java/com/example/mikel/gestordepartidos/Partido.java
+++ app/src/main/java/com/example/mikel/gestordepartidos/Partido.java
@@ -3,20 +3,11 @@ package com.example.mikel.gestordepartidos;
 
 public class Partido {
 
-    /*private Equipo equipo;
-    private Entrenador arbitro;
-    private Entrenador mesa;
-    private Hora hora;*/
-
     private String equipo;
     private String arbitro;
     private String mesa;
     private String hora;
 
-
-    public Partido() {
-        super();
-    }
 
     public Partido(String e, String ar, String m, String h) {
         equipo = e;
@@ -57,47 +48,5 @@ public class Partido {
     public void setHora(String h) {
         hora = h;
     }
-
-    /*public Partido(Equipo e, Entrenador ar, Entrenador m, Hora h) {
-
-        equipo = e;
-        arbitro = ar;
-        mesa = m;
-        hora = h;
-
-    }
-
-    public Equipo getEquipo() {
-        return equipo;
-    }
-
-    public void setEquipo(Equipo e) {
-        equipo = e;
-    }
-
-    public Entrenador getArbitro() {
-        return arbitro;
-    }
-
-    public void setArbitro(Entrenador ar) {
-        arbitro = ar;
-    }
-
-    public Entrenador getMesa() {
-        return mesa;
-    }
-
-    public void setMesa(Entrenador m) {
-        mesa = m;
-    }
-
-    public Hora getHora() {
-        return hora;
-    }
-
-    public void setHora(Hora h) {
-        hora = h;
-    }
-*/
 
 }
